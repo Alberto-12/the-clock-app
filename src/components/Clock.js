3d78cf44a3b2748abe8d2edf6994aed11d64246c
Alberto Serafini <ilgumo@gmail.com>
2023-05-12 12:03:28 -0400

    expiremnting with icons

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,11 +1,11 @@
-import Refresh from "../assets/desktop/icon-refresh.svg"
+
 
 const Clock = () => {
     return (
         <div id="container">
             <div>
             <p>“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”</p>
-            <button>{Refresh}</button>
+            <button></button>
             </div>
             <p>Ada Lovelace</p>
             
