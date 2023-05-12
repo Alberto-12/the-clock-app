7ad5d766be1903912f13158bcade60431782518c
Alberto Serafini <ilgumo@gmail.com>
2023-05-12 16:05:29 -0400

    put the fonts backrground images and some dummy content

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,11 +3,12 @@
 const Clock = () => {
     return (
         <div id="container">
-            <div>
-            <p>“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”</p>
-            <button></button>
+            <div id="quote-generator-div">
+            <p id="quote">“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”</p>
+            <div id="button"></div>
             </div>
-            <p>Ada Lovelace</p>
+            <p id="author">Ada Lovelace</p>
+            
             
         </div>
     )
