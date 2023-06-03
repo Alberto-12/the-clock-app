320ce263657eb52ea7cf4600a8fe84b8784fd33c
Alberto Serafini <ilgumo@gmail.com>
2023-06-03 11:13:50 -0400

    working on the less div

--- src/components/less-div/Less.js
+++ src/components/less-div/Less.js
@@ -2,17 +2,30 @@ import "./Less.css";
 
 const Less = () => {
     return (
-        <div id="less-div">
+        <div id="less-div" class="container">
+            <div className="section-container">
+          <div className="section">  
           <p className="left-column">current time zone</p>
           <p className="right-column">Europe/London</p>
+          </div>
+          <div div className="section"> 
           <p className="left-column">day of the year</p>
           <p className="right-column numbers">295</p>
+          </div>
+          </div>
+          <div className="section-container">
+          <div div className="section"> 
           <p className="left-column">day of the week</p>
           <p className="right-column numbers">5</p>
+          </div>
+          <div div className="section"> 
           <p className="left-column">week number</p>
           <p className="right-column numbers">42</p>
+          </div>
+          </div>
+          
         </div>
     )
 }
 
-export default Less
+export default Less;
