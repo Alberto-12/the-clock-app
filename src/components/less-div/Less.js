18799f38d452a7e6d796ce867c58390084d35db0
Alberto Serafini <ilgumo@gmail.com>
2023-07-17 17:07:59 -0400

    working on media queries for desktop

--- src/components/less-div/Less.js
+++ src/components/less-div/Less.js
@@ -12,16 +12,16 @@ const Less = ({ darkTheme, showLess }) => {
           <p className="left-column">current time zone</p>
           <p className="right-column">Europe/London</p>
         </div>
-        <div div className="section">
+        <div className="section" id="day-of-the-year">
           <p className="left-column">day of the year</p>
           <p className="right-column numbers">295</p>
         </div>
 
-        <div div className="section">
+        <div className="section">
           <p className="left-column">day of the week</p>
           <p className="right-column numbers">5</p>
         </div>
-        <div div className="section" id="week-number">
+        <div className="section" id="week-number">
           <p className="left-column">week number</p>
           <p className="right-column numbers">42</p>
         </div>
--- src/components/more-div/More.js
+++ src/components/more-div/More.js
@@ -7,33 +7,32 @@ const More = ({ toggleLess, toggleGenerator }) => {
 
   const toggleExpansion = () => {
     setIsExpanded(!isExpanded);
-      toggleGenerator();
-    
+    toggleGenerator();
   };
 
   return (
     <div id="more-container">
-      <div id="greeting-div">
-        <div id="icon"></div>
-        <p id="greeting">GOOD MORNING</p>
-      </div>
-      <div id="time-zone-div">
-        <div id="time">11:37</div>
-        <p id="timeZone">BST</p>
-      </div>
-      <div id="city-button-div">
-        <p className="clock" id="location">
-          IN LONDON, UK
-        </p>
-        <div id="more-button-div" onClick={toggleExpansion}>
-          <button id="more-button" onClick={toggleExpansion}>
-            {isExpanded ? "less" : "more"}
-          </button>
-          <div id="arrow-down-div" className={isExpanded ? "rotate-up" : ""}>
-            <img id="arrow-down" src={ArrowDown} alt="arrow down" />
+        <div id="greeting-div">
+          <div id="icon"></div>
+          <p id="greeting">GOOD MORNING</p>
+        </div>
+        <div id="time-zone-div">
+          <div id="time">11:37</div>
+          <p id="timeZone">BST</p>
+        </div>
+        <div id="city-button-div">
+          <p className="clock" id="location">
+            IN LONDON, UK
+          </p>
+          <div id="more-button-div" onClick={toggleExpansion}>
+            <button id="more-button" onClick={toggleExpansion}>
+              {isExpanded ? "less" : "more"}
+            </button>
+            <div id="arrow-down-div" className={isExpanded ? "rotate-up" : ""}>
+              <img id="arrow-down" src={ArrowDown} alt="arrow down" />
+            </div>
           </div>
         </div>
-      </div>
     </div>
   );
 };
